a76a5e2119b0e36f72b32fbf89636899f9932dcc
Adrian Witaszak <adrianwitaszak@gmail.com>
2021-11-07 13:43:45 +0000

    created

---
+++ pex/usecases/build/tmp/kapt3/stubs/debug/com/adwi/interactors/TestKt.java
@@ -0,0 +1,14 @@
+package com.adwi.interactors;
+
+import java.lang.System;
+
+@kotlin.Metadata(mv = {1, 5, 1}, k = 2, d1 = {"\u0000\n\n\u0000\n\u0002\u0010\u000e\n\u0002\b\u0003\"\u0011\u0010\u0000\u001a\u00020\u0001\u00a2\u0006\b\n\u0000\u001a\u0004\b\u0002\u0010\u0003\u00a8\u0006\u0004"}, d2 = {"baseUrl", "", "getBaseUrl", "()Ljava/lang/String;", "usecases_debug"})
+public final class TestKt {
+    @org.jetbrains.annotations.NotNull()
+    private static final java.lang.String baseUrl = null;
+    
+    @org.jetbrains.annotations.NotNull()
+    public static final java.lang.String getBaseUrl() {
+        return null;
+    }
+}
