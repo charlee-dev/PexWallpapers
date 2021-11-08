e870d82032ac69a8b267aef5d1c2b8e78ae9dacd
Adrian Witaszak <adrianwitaszak@gmail.com>
2021-11-08 17:18:22 +0000

    added wallpaper item

--- composables/src/main/java/com/adwi/camposables/WallpaperList.kt
+++ composables/src/main/java/com/adwi/camposables/WallpaperList.kt
@@ -1,16 +1,26 @@
 package com.adwi.camposables
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import com.adwi.camposables.test.TAG_HERO_NAME
+import com.adwi.camposables.test.TAG_HERO_PRIMARY_ATTRIBUTE
 import com.adwi.core.domain.ProgressBarState
 import com.adwi.core.domain.WallpaperListState
+import com.adwi.domain.Wallpaper
 
 @Composable
 fun WallpaperList(
@@ -24,13 +34,82 @@ fun WallpaperList(
                 .fillMaxSize()
         ) {
             items(state.wallpapers) { wallpaper ->
-                Text(text = wallpaper.photographer)
+                WallpaperListItem(
+                    wallpaper = wallpaper,
+                    onSelectHero = {}
+                )
             }
         }
         if (state.progressBarState is ProgressBarState.Loading) {
             CircularProgressIndicator(
                 modifier = Modifier.align(Alignment.Center)
             )
+        }
+    }
+}
+
+@Composable
+fun WallpaperListItem(
+    wallpaper: Wallpaper,
+    onSelectHero: (Long) -> Unit,
+    // imageLoader: ImageLoader, // TODO
+) {
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(bottom = 8.dp)
+            .background(MaterialTheme.colors.surface)
+            .clickable {
+                onSelectHero(wallpaper.id)
+            },
+        elevation = 8.dp
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Box(
+                // TODO(Replace with Image)
+                modifier = Modifier
+                    .width(120.dp)
+                    .height(70.dp)
+                    .background(Color.LightGray),
+            )
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth(.8f) // fill 80% of remaining width
+                    .padding(start = 12.dp)
+            ) {
+                Text(
+                    modifier = Modifier
+                        .padding(bottom = 4.dp)
+                        .testTag(TAG_HERO_NAME),
+                    text = wallpaper.photographer,
+                    style = MaterialTheme.typography.h4,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis
+                )
+                Text(
+                    modifier = Modifier.testTag(TAG_HERO_PRIMARY_ATTRIBUTE),
+                    text = wallpaper.categoryName,
+                    style = MaterialTheme.typography.subtitle1,
+                )
+            }
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth() // Fill the rest of the width (100% - 80% = 20%)
+                    .padding(end = 12.dp),
+                horizontalAlignment = Alignment.End
+            ) {
+                // Using remember in list item does not behave correctly?
+//                val proWR: Int = remember{round(hero.proWins.toDouble() / hero.proPick.toDouble() * 100).toInt()}
+                Text(
+                    text = wallpaper.updatedAt.toString(),
+                    style = MaterialTheme.typography.caption,
+                    color = MaterialTheme.colors.error,
+                )
+            }
         }
     }
 }
