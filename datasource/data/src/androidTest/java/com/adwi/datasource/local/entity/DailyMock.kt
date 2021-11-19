8125d8ab931c4ee684561e4ae2f1299c22ddc28c
Adrian Witaszak <adrianwitaszak@gmail.com>
2021-11-19 05:54:50 +0000

    created tests

---
+++ datasource/data/src/androidTest/java/com/adwi/datasource/local/entity/DailyMock.kt
@@ -0,0 +1,10 @@
+package com.adwi.datasource.local.entity
+
+import com.adwi.datasource.local.domain.DailyEntity
+
+object DailyMock {
+
+    val first = DailyEntity(wallpaperId = 1)
+
+    val second = DailyEntity(wallpaperId = 2)
+}
