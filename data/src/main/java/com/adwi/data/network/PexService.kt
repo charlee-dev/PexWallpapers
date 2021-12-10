f033cabb852411e0c0de0bb2d8bbf04d851801ad
Adrian Witaszak <adrianwitaszak@gmail.com>
2021-12-10 19:29:03 +0000

    swapped Lottie image with Icon

--- data/src/main/java/com/adwi/data/network/PexService.kt
+++ data/src/main/java/com/adwi/data/network/PexService.kt
@@ -10,20 +10,20 @@ import retrofit2.http.Query
 
 interface PexService {
 
-    @GET("v1/search")
+    @GET("search")
     suspend fun getDailyWallpapers(
         @Query("query") categoryName: String = DEFAULT_DAILY_CATEGORY,
         @Query("page") page: Int = 1,
         @Query("per_page") perPage: Int = DAILY_PAGE_SIZE
     ): WallpaperResponse
 
-    @GET("v1/curated")
+    @GET("curated")
     suspend fun getCuratedWallpapers(
         @Query("page") page: Int = 1,
         @Query("per_page") perPage: Int = CURATED_PAGE_SIZE
     ): WallpaperResponse
 
-    @GET("v1/search")
+    @GET("search")
     suspend fun getSearch(
         @Query("query") query: String,
         @Query("page") page: Int = 1,
